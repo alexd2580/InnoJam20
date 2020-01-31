e8d072ccce96a1b666293e6f31fadfb3bf2d56b2
Arne Beer <contact@arne.beer>
2020-01-31 22:54:52 +0100

    Fix asteroid spawning and Vector class usage

--- src/systems/game/AsteroidSpawnSystem.lua
+++ src/systems/game/AsteroidSpawnSystem.lua
@@ -1,3 +1,4 @@
+local Vector = require("helper/Vector")
 local Body, Color, DrawableCircle = Component.load({"Body", "Color", "DrawableCircle"})
 local AsteroidSpawnSystem = class("AsteroidSpawnSystem", System)
 
@@ -13,16 +14,41 @@ function AsteroidSpawnSystem:update(dt)
     local engine = stack:current().engine
     if self.timer > self.spawntime then
         self.timer = self.timer - self.spawntime
+
+        local x, y = math.random(-200, 200), math.random(-200, 200)
+        -- Spawn the asteroids at least -+50 left/right outside the viewport
+        if x < 0 then
+            x = -50 + x
+        else
+            x = 1970 + x
+        end
+
+        -- Spawn the asteroids at least -+50 top/down outside the viewport
+        if y < 0 then
+            y = -50 + x
+        else
+            y = 1130 + x
+        end
+
+        local position = Vector(x, y)
+        -- Target any random place within the viewport (excluding the outer 100 coordinate square)
+        local target = Vector(math.random(100, 1820), math.random(100, 980))
+        local motionVector = target:subtract(position)
+
         -- Create a new entity with a physics body
         local asteroid = Entity()
-        local body = love.physics.newBody(world, 400, 400, "dynamic")
+        local body = love.physics.newBody(world, position.x, position.y, "dynamic")
         local shape = love.physics.newCircleShape(20)
         local fixture = love.physics.newFixture(body, shape)
         asteroid:add(Body(body))
-        body:setLinearVelocity(math.random(-50, 50), math.random(-50, 50))
+
+        local velocity = math.random(100, 200)
+        motionVector = motionVector:getUnit()
+        motionVector = motionVector:multiply(velocity)
+        body:setLinearVelocity(motionVector.x, motionVector.y)
 
         -- Add drawing stuff
-        asteroid:add(DrawableCircle(50, true))
+        asteroid:add(DrawableCircle(20, true))
         asteroid:add(Color(255, 255, 0))
 
         engine:addEntity(asteroid)
